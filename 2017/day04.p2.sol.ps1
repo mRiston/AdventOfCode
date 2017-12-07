76f376e05bb5a524ea9f4f762213f8a426ad7ace
mRiston <mikeriston+GitHub@gmail.com>
2017-12-07 16:10:51 -0500

    Days 4 and 5

---
+++ 2017/day04.p2.sol.ps1
@@ -0,0 +1,33 @@
+﻿function Get-AOCInput ($path) {
+  #$content = 
+  Get-Content $path
+  #return $content
+}
+
+$content = Get-AOCInput -path .\day04.input
+$validPhrases = 0
+$validPWds = @()
+foreach ($phrase in $content) {
+  $phraseWords = $phrase -split '\s'
+  if (($phraseWords | select -Unique).count -eq $phraseWords.count) { $validPhrases++ ; $validPWds += $phrase } 
+}
+
+Write-Host "Day 04 Part 1 Solution: " -NoNewline;Write-Host "$validPhrases" -ForegroundColor Green
+
+$valid = 0
+foreach ($phrase in $validPWDs) {
+  $phraseWords = $phrase -split '\s'
+  
+  $sortedphrasewords = $null
+  foreach ($word in $phraseWords) {
+    $sortedPhraseWords = @($(Join-String ($word -split '' | sort))) + $sortedphrasewords
+  }
+  $sortedphrasewords = $sortedphrasewords -split '\s'
+  $badphrase = $false
+  
+  foreach ($word in $sortedphrasewords -and !$badphrase) {
+    if (($sortedphrasewords | Select-Object -Unique).count -eq $sortedphrasewords.count) { $valid++ }
+    ELSE {$badphrase = $true}
+   } 
+}
+Write-Host "Day 04 Part 2 Solution: " -NoNewline;Write-Host $valid -ForegroundColor Green
---
+++ 2017/day04.sol.ps1
@@ -0,0 +1,15 @@
+﻿function Get-AOCInput ($path) {
+  #$content = 
+  Get-Content $path
+  #return $content
+}
+
+$content = Get-AOCInput -path .\day04.input
+$validPhrases = 0
+
+foreach ($phrase in $content) {
+  $phraseWords = $phrase -split '\s'
+  if (($phraseWords | select -Unique).count -eq $phraseWords.count) { $validPhrases++ } 
+}
+
+Write-Host "Day 04 Part 1 Solution: " -NoNewline;Write-Host "$validPhrases" -ForegroundColor Green
