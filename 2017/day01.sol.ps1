4440efb48da82802c3f8ae121c006f58714ff190
mRiston <mikeriston+GitHub@gmail.com>
2017-12-07 10:52:02 -0500

    day01 commit

---
+++ 2017/day01.sol.ps1
@@ -0,0 +1,20 @@
+﻿[string]$challenge = Get-Content .\day01.input
+$pos = 0
+[int]$sum = 0
+[string]$numtoadd=$challenge.ToString()[$pos] #Store the beginning number
+$pos++ # Advance to the second digit - we've already stored the first
+while ($pos -lt $challenge.ToString().Length) {
+  if ($challenge.ToString()[$pos] -eq $numtoadd) {
+    # Success - we've found a match.
+    # Add it to our summary array
+    $sum += $numtoadd
+    # Advance $pos to next Digit an extra time and store that in $numtoadd
+    $pos++ 
+      $numtoadd = $challenge.ToString()[$pos] 
+    } else {
+      #Do nothing, the number in $pos doesn't match
+    }
+    #Advance $pos for while loop
+    $pos++
+}
+$sum
