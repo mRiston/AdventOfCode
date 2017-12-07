858aa49506202ad92916fe934ce54bff115bd5ea
mRiston <mikeriston+GitHub@gmail.com>
2017-12-07 12:03:45 -0500

    day01.part2.solution

---
+++ 2017/day01.p2.sol.ps1
@@ -0,0 +1,28 @@
+﻿$challenge = Get-Content .\day01.input
+$sum = 0
+for ($i=0;$i -lt $challenge.Length; $i++) {
+  $numtotest = $challenge[$i]
+  <#
+  Write-Host "Testing number   : $numtotest" -ForegroundColor Green -NoNewline
+  Write-host " at index $i" -ForegroundColor Cyan
+  #>
+  $testingIndex = $i+($challenge.Length)/2
+
+  if ($testingIndex -gt ( $challenge.Length - 1) ) { 
+    $testingIndex = $testingIndex - $challenge.Length 
+  }
+  
+  $numtocompare = $challenge[$testingIndex]
+  <#Write-Host "Comparing against: $numtocompare at index " -NoNewline
+  Write-Host  $testingIndex -ForegroundColor Cyan#>
+  
+  $testResult = $numtocompare -eq $numtotest
+  <#Write-host 'Test validation ' -NoNewline
+  Write-Host $testResult -ForegroundColor Green#>
+
+  if ($testResult) {$sum += [int]$numtotest.ToString()}
+  <#Write-Host 'current sum' -NoNewline
+  Write-Host $sum -ForegroundColor Red
+  Write-Host "" #>
+}
+$sum 
